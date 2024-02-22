3728f29e34618fbce42a0506ff9dc059dc7eb3ce
eespinoz4 <eespinoza@netcore.cl>
2024-02-22 17:11:58 -0300

    Validacion de Autentificacion Try/Except

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
-
 # Libreria de django con formulario de creacion de usuario
 from django.contrib.auth.forms import UserCreationForm
+# Genera un modelo de usuario automatico a traves de POST
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+# from http.client import HTTPResponse
 
 
 # Views.
-def helloworld(request):
-    return render(request, 'signup.html', {
-        'form': UserCreationForm
-    })
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password1']:
+            try:
+                #Register User TODO: MINUTO 29:20 VIDEO
+                user = User.objects.create_user(username=request.POST['username'], 
+                password=request.POST['password1'])
+                user.save()
+                return HttpResponse('User created successfully') 
+            except:
+                return HttpResponse('Username already exist')
+        return HttpResponse('Password do not match')
